c0919deee1bedde6084c53b34ae445750f157ec6
caddyz <18784227116@163.com>
2019-01-17 00:25:11 +0800

    1.17

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,6 @@
 <template>
     <div id="home">
+        <blog-rank title="最热"></blog-rank>
         <blog-notice></blog-notice>
         <div style="height: 1em"></div>
         <article-info></article-info>
@@ -9,11 +10,13 @@
 <script>
     import BlogNotice from "./homepage/BlogNotice";
     import ArticleInfo from "./homepage/ArticleInfo";
+    import BlogRank from "./homepage/BlogRank";
 
     export default {
         components:{
             ArticleInfo,
-            BlogNotice
+            BlogNotice,
+            BlogRank
         }
     }
 </script>
--- src/components/homepage/BlogRank.vue
+++ src/components/homepage/BlogRank.vue
@@ -1,10 +1,37 @@
 <template>
-    
+    <el-table :data="rankData" stripe style="width: 100%">
+        <el-table-column prop="artTitle" :label="title" width="180"></el-table-column>
+        <el-table-column>
+            <template slot-scope="scope">
+                <el-button @click="check(scope.row)" type="text" size="small">
+                    查看
+                </el-button>
+            </template>
+        </el-table-column>
+    </el-table>
 </template>
 
 <script>
     export default {
-        name: "BlogRank"
+        name: "BlogRank",
+        prop:['title'],
+        data(){
+            return {
+                rankData:[
+                    {artId:"1",artTitle:"啥技术的1"},
+                    {artId:"2",artTitle:"啥技术的2"},
+                    {artId:"3",artTitle:"啥技术的3"},
+                    {artId:"4",artTitle:"啥技术的4"},
+                    {artId:"5",artTitle:"啥技术的5"},
+                    {artId:"6",artTitle:"啥技术的6"},
+                ]
+            }
+        },
+        methods:{
+            check:function (row) {
+                alert(row.artId)
+            }
+        }
     }
 </script>
 
